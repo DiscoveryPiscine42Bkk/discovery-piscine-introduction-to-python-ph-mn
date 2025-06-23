383607b854e3cf5ec888d473ec47af26830dddb1
smyat-na <66011204@kmitl.ac.th>
2025-06-23 21:12:00 +0700

    ex13

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,3 @@
+first_input= input("Hey, what's your first name? : ").strip( )
+last_input = input("And your last name? : ").strip( )
+print (f'Well, pleased to meet you, {first_input} {last_input}.\n')
