d387b58b8a5df88d2b2caf5d647b1c53e9a32502
smyat-na <66011204@kmitl.ac.th>
2025-06-25 03:43:53 +0700

    valueerror

--- cell04/ex04/roundup.py
+++ cell04/ex04/roundup.py
@@ -1,4 +1,8 @@
-import math
+try:
+    import math
 
-number = float(input("Enter a number: "))
-print(math.ceil(number))
+    number = float(input("Enter a number: "))
+    print(math.ceil(number))
+    
+except ValueError:
+    print("Invalid input. Please enter a numeric value.")
