237cce3e209d57822ee15c09482c0bdb143f67a2
smyat-na <66011204@kmitl.ac.th>
2025-06-25 03:46:46 +0700

    tryexcept

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -1,7 +1,10 @@
-a = int(input("Give me your first: "))
-b = int(input("Give me your second: "))
-print("Thank you!")
-print(f"{a} + {b} = {a+b}")
-print(f"{a} - {b} = {a-b}")
-print(f"{a} / {b} = {a/b}")
-print(f"{a} x {b} = {a*b}")
+try:
+    a = int(input("Give me your first: "))
+    b = int(input("Give me your second: "))
+    print("Thank you!")
+    print(f"{a} + {b} = {a+b}")
+    print(f"{a} - {b} = {a-b}")
+    print(f"{a} / {b} = {a/b}")
+    print(f"{a} x {b} = {a*b}")
+except ValueError:
+    print("Invalid input. Please enter a numeric value.")
