fb811de59a5148e4720daac55d76ee5fdea11214
smyat-na <66011204@kmitl.ac.th>
2025-06-25 16:24:49 +0700

    cell510

---
+++ cell05/ex09/scan_it.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+import re
+
+if (len(sys.argv)-1) != 2:
+    print("none")
+else:
+    same = re.findall(sys.argv[1], sys.argv[2])
+    print(len(same))
---
+++ cell05/ex10/parameter_matching.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+
+if (len(sys.argv)) <= 1:
+    print("none")
+else:
+    ask = input("What was the parameter? ")
+    if ask == sys.argv[1]:
+        print("Good job!")
+    else:
+        print("Nope, sorry...")
