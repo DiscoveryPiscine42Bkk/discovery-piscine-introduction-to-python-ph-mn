53c41d5a97185f3ab0f9609b600223c51401c1f0
smyat-na <66011204@kmitl.ac.th>
2025-06-25 21:20:18 +0700

    no end

--- cell05/ex14/free_range.py
+++ cell05/ex14/free_range.py
@@ -4,4 +4,4 @@ import sys
 if len(sys.argv)-1 != 2:
     print("none")
 else:
-    print(list(range(int (sys.argv[1]),int(sys.argv[2])+1)), end="")
+    print(list(range(int (sys.argv[1]),int(sys.argv[2])+1)))
